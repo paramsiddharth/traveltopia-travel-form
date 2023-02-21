4fb8717880cfa35d30725949e731a02be059ca52
Param Siddharth <contact@paramsid.com>
2023-02-22 00:23:54 +0530

    Add .env files
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
+
 const fs = require('fs-extra');
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 const controller = require('./controller');
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
+}
 
 app.use('/api', controller);
 
