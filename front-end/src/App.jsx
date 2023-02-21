1896807d49ae3fea05727bc42296dd4fc99325c0
Param Siddharth <contact@paramsid.com>
2023-02-21 16:53:17 +0530

    Build form UI
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,104 @@
 import { useState } from 'react';
+import {
+  Button,
+  Chip,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  TextField
+} from '@mui/material';
+
 import './App.css';
 
+const places = [
+  'India',
+  'Africa',
+  'Europe'
+];
+
 function App() {
+  const [destination, setDestination] = useState(null);
+
+  const onChange = set => e => set(e.target.value);
+
   return (
     <div className="App">
-      <div>
-      </div>
-      <h1>Travel Form</h1>
+      <h1>Travelopia</h1>
+      <h2>Submission Form</h2>
       <div>
         <form>
-              
+          <p>
+            Contact us to plan your next journey!
+          </p>
+          <div className='input-field'>
+            <TextField
+              variant='filled'
+              fullWidth
+              label='Name'
+              required
+            />
+          </div>
+          <div className='input-field'>
+            <TextField
+              variant='filled'
+              fullWidth
+              label='E-mail address'
+              type='email'
+              required
+            />
+          </div>
+          <div className='input-field'>
+            <FormControl variant='filled' sx={{ width: '100%', textAlign: 'left' }}>
+              <InputLabel id='destination' mt={3}>Where do you want to go? *</InputLabel>
+              <Select
+                labelId='destination'
+                required
+                value={destination}
+                onChange={onChange(setDestination)}
+              >
+                {places.map(place => (
+                  <MenuItem key={place} value={place}>
+                    { place }
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+          <div className='input-field'>
+            <TextField
+              variant='filled'
+              fullWidth
+              label='Number of travellers'
+              type='number'
+              required
+            />
+          </div>
+          <div className='input-field'>
+            <TextField
+              InputProps={{
+                startAdornment: <InputAdornment position='start'>$</InputAdornment>
+              }}
+              variant='filled'
+              fullWidth
+              label='Budget per person'
+              type='number'
+              required
+            />
+          </div>
+          <div className='input-field submit'>
+            <Chip label='Net budget: $400' onClick={() => {}} />
+            <Button
+              sx={{
+                paddingRight: 4,
+                paddingLeft:  4
+              }}
+              variant='contained'
+              color='primary'
+            >Submit</Button>
+          </div>
         </form>
       </div>
     </div>
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material';
+
 import App from './App';
 import './index.css';
 
+const themes = {
+  light: createTheme({
+    palette: {
+      mode: 'light'
+    }
+  }),
+  dark: createTheme({
+    palette: {
+      mode: 'dark'
+    }
+  })
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themes.dark}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
